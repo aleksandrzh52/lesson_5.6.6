4e20bf053623be1d3ee799e4cf5bfc9d52f143d8
aleksandrzh52 <96547517+aleksandrzh52@users.noreply.github.com>
2022-01-23 19:38:32 +0300

    Byyy

--- src/Num.java
+++ src/Num.java
@@ -1,7 +1,11 @@
 public class Num {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            if(i%2==0) System.out.println(i);
+            if(i%2==0) {
+                if(i%10==0) System.out.print("Byyy   ");
+                System.out.println(i);
+            }
+
 
         }
     }
